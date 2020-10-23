5e59108004816a6cbd292261eec69fb2a45c1cb4
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2020-10-23 12:48:29 +0200

    improved cache handling

--- src/main/java/de/netbeacon/xenia/backend/client/objects/internal/ws/WebSocketListener.java
+++ src/main/java/de/netbeacon/xenia/backend/client/objects/internal/ws/WebSocketListener.java
@@ -16,6 +16,7 @@
 
 package de.netbeacon.xenia.backend.client.objects.internal.ws;
 
+import de.netbeacon.utils.executor.ScalingExecutor;
 import de.netbeacon.utils.shutdownhook.IShutdown;
 import de.netbeacon.xenia.backend.client.core.XeniaBackendClient;
 import de.netbeacon.xenia.backend.client.objects.external.Channel;
@@ -37,16 +38,21 @@ public class WebSocketListener extends okhttp3.WebSocketListener implements IShu
 
     private final Logger logger = LoggerFactory.getLogger(WebSocketListener.class);
     private final XeniaBackendClient xeniaBackendClient;
+    private ScalingExecutor scalingExecutor;
     private WebSocket webSocket;
 
     public WebSocketListener(XeniaBackendClient xeniaBackendClient){
         this.xeniaBackendClient = xeniaBackendClient;
     }
 
-    public WebSocketListener start(){
+    public void start(){
+        if(scalingExecutor != null){
+            scalingExecutor.shutdown();
+        }
         if(webSocket != null){
             webSocket.close(1000, "Reconnecting Soon");
         }
+        scalingExecutor = new ScalingExecutor(2, 14, 12000, 30, TimeUnit.SECONDS);
         BackendSettings backendSettings = xeniaBackendClient.getBackendProcessor().getBackendSettings();
         String host = backendSettings.getHost();
         int port = backendSettings.getPort();
@@ -54,13 +60,13 @@ public class WebSocketListener extends okhttp3.WebSocketListener implements IShu
         // build request
         Request request = new Request.Builder().url("wss://"+host+":"+port+"/ws?token="+token).build();
         webSocket = xeniaBackendClient.getOkHttpClient().newWebSocket(request, this);
-        return this;
     }
 
-    public WebSocketListener stop(){
+    public void stop(){
+        scalingExecutor.shutdown();
+        scalingExecutor = null;
         webSocket.close(1000, "Closed Connection");
         webSocket = null;
-        return this;
     }
 
     @Override
@@ -88,7 +94,7 @@ public class WebSocketListener extends okhttp3.WebSocketListener implements IShu
     public void onClosed(@NotNull WebSocket webSocket, int code, @NotNull String reason) {
         logger.debug("Websocket Closed: "+code+" "+reason);
         if(code != 1000){
-            logger.warn("Reconnecting On: "+code);
+            logger.debug("Reconnecting On: "+code);
             start(); // reconnect
         }
     }
@@ -106,70 +112,159 @@ public class WebSocketListener extends okhttp3.WebSocketListener implements IShu
         try{
             String type = message.getString("type");
             String action = message.getString("action");
+            logger.debug("Received Message From WS: "+message.toString());
             switch (type.toLowerCase()){
                 case "status":
                     logger.info("Received Status From WS: "+action);
                     break;
                 case "user":
+                {
                     if(!xeniaBackendClient.getUserCache().contains(message.getLong("userId"))){
                         return;
-                    }else if(!action.equalsIgnoreCase("create")){
-                        xeniaBackendClient.getUserCache().remove(message.getLong("userId"));
                     }
-                    break;
+                    switch (action.toLowerCase()){
+                        case "create":
+                            break;
+                        case "update":
+                            xeniaBackendClient.getUserCache().get(message.getLong("userId")).getAsync();
+                            break;
+                        case "delete":
+                            xeniaBackendClient.getUserCache().remove(message.getLong("userId"));
+                            break;
+                    }
+                }
+                break;
                 case "guild":
+                {
                     if(!xeniaBackendClient.getGuildCache().contains(message.getLong("guildId"))){
                         return;
-                    }else if(!action.equalsIgnoreCase("create")){
-                        xeniaBackendClient.getGuildCache().remove(message.getLong("guildId"));
                     }
-                    break;
+                    switch (action.toLowerCase()){
+                        case "create":
+                            break;
+                        case "update":
+                            xeniaBackendClient.getGuildCache().get(message.getLong("guildId")).getAsync(); // this just gets the new data as we dont want to reload all channels, roles, members,...
+                            break;
+                        case "delete":
+                            xeniaBackendClient.getGuildCache().remove(message.getLong("guildId"));
+                            break;
+                    }
+                }
+                break;
                 case "guild_role":
                 case "guild_role_permission":
+                {
                     if(!xeniaBackendClient.getGuildCache().contains(message.getLong("guildId"))){
                         return;
-                    }else if(!action.equalsIgnoreCase("create")){
-                        Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
-                        g.getRoleCache().remove(message.getLong("roleId"));
                     }
-                    break;
+                    Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
+                    switch (action.toLowerCase()){
+                        case "create":
+                            scalingExecutor.execute(()->g.getRoleCache().get(message.getLong("roleId")));
+                            break;
+                        case "update":
+                            g.getRoleCache().remove(message.getLong("roleId"));
+                            scalingExecutor.execute(()->g.getRoleCache().get(message.getLong("roleId")));
+                            break;
+                        case "delete":
+                            g.getRoleCache().remove(message.getLong("roleId"));
+                            break;
+                    }
+                }
+                break;
                 case "guild_channel":
+                {
                     if(!xeniaBackendClient.getGuildCache().contains(message.getLong("guildId"))){
                         return;
-                    }else if(!action.equalsIgnoreCase("create")){
-                        Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
-                        g.getChannelCache().remove(message.getLong("channelId"));
                     }
-                    break;
+                    Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
+                    switch (action.toLowerCase()){
+                        case "create":
+                            scalingExecutor.execute(()->g.getChannelCache().get(message.getLong("channelId")));
+                            break;
+                        case "update":
+                            g.getChannelCache().remove(message.getLong("channelId"));
+                            scalingExecutor.execute(()->g.getChannelCache().get(message.getLong("channelId")));
+                            break;
+                        case "delete":
+                            g.getChannelCache().remove(message.getLong("channelId"));
+                            break;
+                    }
+                }
+                break;
                 case "guild_message":
+                {
                     if(!xeniaBackendClient.getGuildCache().contains(message.getLong("guildId"))){
                         return;
-                    }else if(!action.equalsIgnoreCase("create")){
-                        Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
-                        if(!g.getChannelCache().contains(message.getLong("channelId"))){
-                            return;
-                        }
-                        Channel c = g.getChannelCache().get(message.getLong("channelId"));
-                        c.getMessageCache().remove(message.getLong("messageId"));
                     }
-                    break;
+                    Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
+                    if(!g.getChannelCache().contains(message.getLong("channelId"))){
+                        return;
+                    }
+                    Channel c = g.getChannelCache().get(message.getLong("channelId"));
+                    switch (action.toLowerCase()){
+                        case "create":
+                            scalingExecutor.execute(()->c.getMessageCache().get(message.getLong("messageId")));
+                            break;
+                        case "update":
+                            c.getMessageCache().remove(message.getLong("messageId"));
+                            scalingExecutor.execute(()->c.getMessageCache().get(message.getLong("messageId")));
+                            break;
+                        case "delete":
+                            c.getMessageCache().remove(message.getLong("messageId"));
+                            break;
+                    }
+                }
+                break;
                 case "guild_license":
+                {
                     if(!xeniaBackendClient.getLicenseCache().contains(message.getLong("guildId"))){
                         return;
                     }else{
                         xeniaBackendClient.getLicenseCache().remove(message.getLong("guildId"));
                     }
-                    break;
+                }
+                break;
                 case "guild_member":
+                {
                     if(!xeniaBackendClient.getGuildCache().contains(message.getLong("guildId"))){
                         return;
-                    }else if(!action.equalsIgnoreCase("create")){
-                        Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
-                        g.getMemberCache().remove(message.getLong("userId"));
                     }
-                    break;
+                    Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
+                    switch (action.toLowerCase()){
+                        case "create":
+                            scalingExecutor.execute(()->g.getMemberCache().get(message.getLong("userId")));
+                            break;
+                        case "update":
+                            g.getMemberCache().remove(message.getLong("userId"));
+                            scalingExecutor.execute(()->g.getMemberCache().get(message.getLong("userId")));
+                            break;
+                        case "delete":
+                            g.getMemberCache().remove(message.getLong("userId"));
+                            break;
+                    }
+                }
+                break;
                 case "guild_misc_tag":
-                    break;
+                {
+                    if(!xeniaBackendClient.getGuildCache().contains(message.getLong("guildId"))){
+                        return;
+                    }
+                    Guild g = xeniaBackendClient.getGuildCache().get(message.getLong("guildId"));
+                    switch (action.toLowerCase()){
+                        case "create":
+                            scalingExecutor.execute(()->g.getMiscCaches().getTagCache().get(message.getString("tagName")));
+                            break;
+                        case "update":
+                            g.getMiscCaches().getTagCache().remove(message.getString("tagName"));
+                            scalingExecutor.execute(()->g.getMiscCaches().getTagCache().get(message.getString("tagName")));
+                            break;
+                        case "delete":
+                            g.getMiscCaches().getTagCache().remove(message.getString("tagName"));
+                            break;
+                    }
+                }
+                break;
             }
         }catch (Exception e){
             logger.warn("Error Processing Message, Cache Might Be Inconsistent: "+message.toString());
