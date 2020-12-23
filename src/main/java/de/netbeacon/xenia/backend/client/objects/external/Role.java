9744373bcd4b9729d7e0cc5726e80d386ab91e50
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2020-12-23 19:04:47 +0100

    updated permissions

--- src/main/java/de/netbeacon/xenia/backend/client/objects/external/Role.java
+++ src/main/java/de/netbeacon/xenia/backend/client/objects/external/Role.java
@@ -96,19 +96,17 @@ public class Role extends APIDataObject {
 
         public enum Bit{
 
-            // UNUSED_PERMISSION_BIT_63(63),
-
+            UNUSED_PERMISSION_BIT_63(63),
             // OWNER
             GUILD_OWNER_OVERRIDE(62),
             // SETTINGS
             GUILD_SETTINGS_OVERRIDE(61),
-            // WEBINTERFACE
-            WEBINTERFACE_USE(60),
             // ROLES
-            GUILD_ROLES_OVERRIDE(59),
+            GUILD_ROLES_OVERRIDE(60),
             // CHANNELS
-            GUILD_CHANNEL_OVERRIDE(58),
+            GUILD_CHANNEL_OVERRIDE(59),
 
+            UNUSED_PERMISSION_BIT_58(58),
             UNUSED_PERMISSION_BIT_57(57),
             UNUSED_PERMISSION_BIT_56(56),
             UNUSED_PERMISSION_BIT_55(55),
