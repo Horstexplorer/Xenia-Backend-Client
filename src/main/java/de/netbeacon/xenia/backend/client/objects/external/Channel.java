b342573da3a691f13de37092aa94d9bdc1aee4e1
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2021-02-09 20:58:49 +0100

    minor fix

--- src/main/java/de/netbeacon/xenia/backend/client/objects/external/Channel.java
+++ src/main/java/de/netbeacon/xenia/backend/client/objects/external/Channel.java
@@ -58,6 +58,7 @@ public class Channel extends APIDataObject {
         this.channelFlags = new ChannelFlags(0);
         this.channelSettings = new ChannelSettings(0);
         this.d43z1Settings = new D43Z1Settings(0);
+        this.d43z1CustomContextPoolUUID = new UUID(0,0);
         setBackendPath("data", "guilds", (Supplier<Long>) this::getGuildId, "channels", (Supplier<Long>) this::getChannelId);
     }
 
